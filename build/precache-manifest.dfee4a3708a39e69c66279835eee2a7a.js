2cc22c4633f58c48e2cc84bb0c675bdbe165bb3d
Thomas Roberts <robertsthomasd@gmail.com>
2021-04-30 22:56:11 -0700

    fix header color and add body content

---
+++ build/precache-manifest.dfee4a3708a39e69c66279835eee2a7a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e2147ac15a94dc1280421ce42f86b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc5a0c84e880889a97f2",
+    "url": "/static/css/main.8b904439.chunk.css"
+  },
+  {
+    "revision": "fc975f47fca27e4d9a02",
+    "url": "/static/js/2.3bddefb0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3bddefb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc5a0c84e880889a97f2",
+    "url": "/static/js/main.6f48d7df.chunk.js"
+  },
+  {
+    "revision": "4398edff805c5e43afc9",
+    "url": "/static/js/runtime-main.91264036.js"
+  },
+  {
+    "revision": "ced97463d0a4d5386f5cdea3e475ca27",
+    "url": "/static/media/huckberry-logo.ced97463.svg"
+  },
+  {
+    "revision": "767335f67b285aff3c1637dc0218d987",
+    "url": "/static/media/icon-bag.767335f6.svg"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Header from './components/Header/Header';
 
-ReactDOM.render(<Header />, document.getElementById('root'));
+ReactDOM.render((
+    <div>
+        <Header />
+        <div style={{ backgroundColor: '#F4F5F5', height: '89vh', display: 'flex', justifyContent: 'center', alignItems: 'center', position: 'absolute', bottom: 0, zIndex: -1, width: '100%'}}>
+            <h1 style={{fontSize: 40}}>Welcome to Huckberry</h1>
+        </div>
+    </div>)
+    , document.getElementById('root'));
